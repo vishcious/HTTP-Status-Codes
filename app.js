134019cba4e56abad83f9684f7719f49032d6b51
Vish Uma <viswaug@gmail.com>
2012-01-22 00:25:27 -0500

    troubleshooting port issue

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ var app = express.createServer();
 require('./config').setup(app);
 require('./routes').setup(app);
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
