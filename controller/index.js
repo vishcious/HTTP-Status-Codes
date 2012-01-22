7e554c0db410583d9d54cb8c2367e03398f7b2d5
Vish Uma <viswaug@gmail.com>
2012-01-22 13:04:05 -0500

    adding a http status code list page

--- controller/index.js
+++ controller/index.js
@@ -6,7 +6,20 @@ module.exports.setRepositoryFactory = function(factory) {
 };
 
 module.exports.home = function(req, res) {
-  res.send('Hello World');
+  res.render('home', {
+    title: "Vish's nodejs web experiments"
+  });
+};
+
+module.exports.statusList = function(req, res) {
+ var repository = repositoryFactory.create('statusCode');
+ var statusList = repository.getAll();
+  res.render('statusList', {
+    title: 'HTTP Status Code List',
+    locals: {
+      list: statusList
+    }
+  });
 };
 
 module.exports.statusCode = function(req, res) {
@@ -22,6 +35,18 @@ module.exports.statusCode = function(req, res) {
       statusName: statusData
     }
   });
+};
+
+
+module.exports.statusReturn = function(req, res) {
+  statusCode = req.params.statusCode;
+  var repository = repositoryFactory.create('statusCode');
+  var statusData = repository.getById(statusCode);
+  if(!statusData)
+    res.send('Invalid Status Code', {}, 404);
+  else if(statusCode.charAt(0) === "1")
+    res.send('1XX status codes are not supported', {}, 404);
+  res.send(parseInt(statusCode));
 };
 
 module.exports.twss = function(req, res) {
--- routes.js
+++ routes.js
@@ -6,7 +6,9 @@ module.exports.setup = function(app) {
   app.param('statusCode', paramPreconditions.statusCode);
 
   app.get('/', controller.home);
+  app.get('/status/', controller.statusList);
   app.get('/status/:statusCode', controller.statusCode);
+  app.get('/status/:statusCode/return', controller.statusReturn);
   app.get('/twss', controller.twss);
   app.post('/twss', controller.processTwss);
 };
